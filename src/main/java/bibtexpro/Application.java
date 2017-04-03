d5e0dc94fa93bf35d69e9fc6aa74fa827a589f09
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2017-04-03 17:21:33 +0300

    lisätty viite domain ja html lisäämiseen

--- src/main/java/bibtexpro/Application.java
+++ src/main/java/bibtexpro/Application.java
@@ -1,4 +1,4 @@
-package main;
+package bibtexpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/bibtexpro/domain/Reference.java
@@ -0,0 +1,17 @@
+package bibtexpro.domain;
+
+import java.util.Map;
+
+public class Reference {
+    
+    private Map<String, String> attributes;
+
+    public Map<String, String> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Map<String, String> attributes) {
+        this.attributes = attributes;
+    }    
+    
+}
