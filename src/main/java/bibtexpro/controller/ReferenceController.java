80a0734f3c0ad2b59e0c552b4a6d2a99ce4fd6fc
Joni Luhtalampi <joni.luhtalampi@gmail.com>
2017-04-04 15:32:32 +0300

    toBibTex

--- src/main/java/bibtexpro/controller/ReferenceController.java
+++ src/main/java/bibtexpro/controller/ReferenceController.java
@@ -1,9 +1,13 @@
 package bibtexpro.controller;
 
+import bibtexpro.domain.Reference;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ReferenceController {
@@ -11,6 +15,19 @@ public class ReferenceController {
     @RequestMapping("/addreference")
     public String addReference(Model model) {
         return "addreference";
+    }
+    
+    @ResponseBody
+    @RequestMapping("/test")
+    public String test(){
+        Map<String, String> map = new HashMap<>();
+        map.put("type", "Book");
+        map.put("id", "testiId");
+        map.put("title", "testiKirja");
+        map.put("author", "Meikalainen, Matti");
+        map.put("year", "2017");
+        Reference ref = new Reference(map);
+        return ref.toBibTex();
     }
 
 }
--- src/main/java/bibtexpro/domain/BibTexParser.java
+++ src/main/java/bibtexpro/domain/BibTexParser.java
@@ -1,14 +1,22 @@
 package bibtexpro.domain;
 
+import java.util.Map;
+
 class BibTexParser {
-    
-    
+
     public static String toBibTex(Reference ref) {
-        
-        return "";
+        StringBuilder b = new StringBuilder("@");
+        b.append(ref.getType() + '{' + ref.getId());
+        Map<String, String> attributes = ref.getAttributes();
+        for (String key : attributes.keySet()) {
+            b.append(",\n");
+            b.append(key + " = " + StringToBibTex(attributes.get(key)));
+        }
+        b.append("\n}\n\n");
+        return b.toString();
     }
-    
-    private String StringToBibTex(String s) {
-        return "";
+
+    private static String StringToBibTex(String s) {
+        return "{"+s+"}";
     }
 }
