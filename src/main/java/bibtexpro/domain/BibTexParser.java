310992a37c8f5aded76326f4de4ead8f3b9b855d
Joni Luhtalampi <joni.luhtalampi@gmail.com>
2017-04-04 14:50:01 +0300

    reference runkoa

---
+++ src/main/java/bibtexpro/domain/BibTexParser.java
@@ -0,0 +1,14 @@
+package bibtexpro.domain;
+
+class BibTexParser {
+    
+    
+    public static String toBibTex(Reference ref) {
+        
+        return "";
+    }
+    
+    private String StringToBibTex(String s) {
+        return "";
+    }
+}
--- src/main/java/bibtexpro/domain/Reference.java
+++ src/main/java/bibtexpro/domain/Reference.java
@@ -5,13 +5,39 @@ import java.util.Map;
 public class Reference {
     
     private Map<String, String> attributes;
+    private String type;
+    private String id;
+
+    public Reference(Map<String, String> inputFields) {
+        this.type = inputFields.get("type");
+        inputFields.remove("type");
+        this.id = inputFields.get("id");
+        inputFields.remove("id");
+        this.attributes = inputFields;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public Map<String, String> getAttributes() {
         return attributes;
     }
 
-    public void setAttributes(Map<String, String> attributes) {
-        this.attributes = attributes;
-    }    
+    public void setAttribute(String key, String value) {
+        this.attributes.put(key, value);
+    }
+    
+    public String toBibTex() {
+        return BibTexParser.toBibTex(this);
+    }
     
 }
