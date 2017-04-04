a23fe356ee6b3f920c769769111b4a33323f4833
Jani Takkinen <jani.takkinen@cs.helsinki.fi>
2017-04-04 15:42:04 +0300

    added controller methods for testing

--- src/main/java/bibtexpro/controller/ReferenceController.java
+++ src/main/java/bibtexpro/controller/ReferenceController.java
@@ -1,5 +1,10 @@
 package bibtexpro.controller;
 
+import bibtexpro.domain.Reference;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +12,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ReferenceController {
-
+    List<Reference> references;
+    
+    
     @RequestMapping("/addreference")
     public String addReference(Model model) {
+        if(references == null){
+            this.references = new ArrayList<>();
+        }
+        Reference newRef = new Reference(this.extractAttributes(model.asMap()));
+        references.add(newRef);
         return "addreference";
+    }
+    
+    private Map<String, String> extractAttributes(Map<String, Object> map){
+        HashMap<String, String> attributes = new HashMap<>();
+        for(String key : map.keySet()){
+            attributes.put(key, map.get(key).toString());
+        }
+        return attributes;
     }
 
 }
