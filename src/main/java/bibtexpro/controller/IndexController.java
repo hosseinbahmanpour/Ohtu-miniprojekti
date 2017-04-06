d4be3535e9bc3b68503a7f91404ab6d8dfe13666
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2017-04-06 16:01:49 +0300

    luokka nimetty oikein

--- src/main/java/bibtexpro/controller/IndexController.java
+++ src/main/java/bibtexpro/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
-public class AddReferenceController {
+public class IndexController {
 
     @RequestMapping("/")
     public String index(Model model) {
