511423fc1ee993d500ff0c6854d8f602c3916dbb
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2017-04-27 20:31:19 +0300

    inproceeding form

---
+++ src/main/java/bibtexpro/domain/InproceedingForm.java
@@ -0,0 +1,162 @@
+package bibtexpro.domain;
+
+import javax.validation.constraints.NotNull;
+
+public class InproceedingForm {
+    
+    @NotNull
+    private String refId;
+
+    @NotNull
+    private String author;
+
+    @NotNull
+    private String title;
+    
+    @NotNull
+    private String booktitle;
+
+    @NotNull
+    private int year;
+
+    private String editor;
+    
+    private int volume;
+	
+    private String series;
+    
+    private int pages;
+    
+    private String address;
+    
+    private int month;
+    
+    private String organization;
+    
+    private String publisher;
+    
+    private String note;
+    
+    private String key;
+
+    public String getRefId() {
+        return refId;
+    }
+
+    public void setRefId(String refId) {
+        this.refId = refId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBooktitle() {
+        return booktitle;
+    }
+
+    public void setBooktitle(String booktitle) {
+        this.booktitle = booktitle;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+    
+}
